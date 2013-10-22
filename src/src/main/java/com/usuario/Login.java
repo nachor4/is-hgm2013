1b40d0ea2eca1053b6ac7aea872b0df36f0d98d8
Nico Gomez <nico.neg88@gmail.com>
2013-10-22 17:31:28 -0300

    cambio de longitud de password y adicion de la funcion sha1

--- src/src/main/java/com/usuario/Login.java
+++ src/src/main/java/com/usuario/Login.java
@@ -2,6 +2,7 @@ package com.usuario;
 
 import com.usuario.models.User;
 
+import org.apache.commons.codec.digest.DigestUtils;
 
 public class Login extends User
 {
@@ -9,28 +10,46 @@ public class Login extends User
 	public Login(){
 		//super();
 	}		
-
 	
-	public boolean check(String usuario, String password) {
-		// Chequea el password del usuario
-		return false;	
-	}
-	
-
 	public static String newUsuario(String id, String password, String name, String email) {
 		// Agrega un Usuario
 		
 		Login login = new Login();
 		
-        login.set("id", id);		
-        login.set("password", password);        
+        login.set("id", id);	
+        
+        DigestUtils digest = new DigestUtils();
+        	
+        login.set("password", DigestUtils.sha1(password));        
         login.set("name", name);
 		login.set("email",email);		
 		
 		login.saveIt();
 		
 		return login.getString("id");
+	}	
+
+	
+	public boolean check(String id, String password) {
+		// Chequea el password del usuario
+		
+		Login login = new Login();
+		
+		if (id == login.id)		
+		
+           DigestUtils digest = new DigestUtils();
+        
+           String utils = digest.sha1(password);
+           
+           if (login.password == utils)
+        		
+		       return true;	
+		   
+		   else return false;
+		   
+		else return false;   
 	}
+
 	
 	
 	public int reset(String usuario, String email) {
