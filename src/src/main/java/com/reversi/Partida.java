d7b4e5da213e482852196ae0f1425e7eda2b1091
guille <guillemorilla85@gmail.com>
2013-10-24 23:46:57 -0300

    se modifico la clase partida

--- src/src/main/java/com/reversi/Partida.java
+++ src/src/main/java/com/reversi/Partida.java
@@ -4,121 +4,86 @@ import com.usuario.UsuarioApp;
 
 import com.usuario.models.User;
 
+import javax.swing.*;
 
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
+import java.sql.Timestamp;
+
+import java.util.Date;
 
 public class Partida
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	private String id;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
 	private double timeUltMov;
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	private byte[] tablero;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
 	public UsuarioApp elNegro;
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	public ReversiObserver reversiObserver;
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
 	public UsuarioApp elBlanco;
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
-	//public Partida(){
-		//super();
-	//}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
+	public Partida(string idNegro, string idBlanco, int dificultRec, ReversiObserver observerRecibido){
+		try {
+	    if(idNegro != null) {
+	     UsuarioApp jugadorNegro = new UsuarioApp();
+	     jugadorNegro = jugadorNegro.findById(idNegro); }
+	     if (jugadorNegro != null)
+			System.out.println ("Jugador encontrado\n");
+	}
+	catch(Execption e){
+	    throw new IllegalArgumentException ("El idNegro recibido no existe\n");
+	    //throw new Exception;
+	    //throw new IllegalArgumentException("Ingreso un ID de USUARIO invalido.");
+	}
 	
+	//Aqui controlo que el idBlanco no sea null
+	try { if(idBlanco != null) {
+		    UsuarioApp jugadorBlanco = new UsuarioApp();	
+		    jugadorBlanco = jugadorBlanco.findById(idBlanco); }
+		    if (jugadorBlanco != null)
+				System.out.println ("Jugador encontrado\n");
+	}
+	catch (Exception e){
+	    throw new IllegalArgumentException ("El idBlanco recibido no existe\n");
+	    //throw new Exception;
+	}
+	
+	//Aqui controlo que el nivel de dificultad sea un valor valido (0, 1 o 2)
+	try{
+		// Que dificultad sea igual a cero significa que el jugador tendra 30 seg para realizar su movimiento
+	    if(dificultRec == 0 || dificultRec == 1 || dificultRec == 2) {
+		   dificultad = dificultRec;  
+		}
+	}
+	catch (Exception e){
+	    throw new IllegalArgumentException ("El nivel de dificultad recibido no es válido\n");
+	    //throw new Exception;
+	}
+	/*
+	try{
+	    //crear el objeto UserApp
+	}
+	catch (Exception e){
+	    //sucedio un errero.
+	   //throw new Exception;
+	} */
+
 	public boolean checkMov() {
 		// TODO : to implement
 		return false;	
 	}
-	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
+		
 	public UsuarioApp jugadorActual() {
 		// TODO : to implement
 		return new UsuarioApp("id");	
 	}
 	
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
 	public int estadoJuego() {
 		// TODO : to implement
 		return 0;	
 	}
-	
-	/**
-	 * ID es igual al nombre de los Usuarios más el TimeStart
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
 	
 	public String getId() {
 		// TODO : to implement
