0cde2998aace93267a4cf7e68c044969696f02b5
Ignacio Herrero <herrero.olivari@gmail.com>
2013-10-30 20:52:32 -0300

    script compilacion s/tests

---
+++ src/ntrun.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+clear
+
+echo "*******************  COLLECTING DEPENDENCIES  *********************************"
+mvn dependency:copy-dependencies
+export CLASPATH=""
+for file in `ls target/dependency`; do export CLASSPATH=$CLASSPATH:target/dependency/$file; done
+export CLASSPATH=$CLASSPATH:target/classes
+
+mvn package embedded-glassfish:run -Dmaven.test.skip=true
