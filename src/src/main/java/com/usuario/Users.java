cfc525dc515168b974fb8f41b7d757021f83b006
Nico Gomez <nico.neg88@gmail.com>
2013-10-30 22:41:32 -0300

    void en connect

--- src/src/main/java/com/usuario/Users.java
+++ src/src/main/java/com/usuario/Users.java
@@ -8,7 +8,7 @@ import org.javalite.activejdbc.Base;
 public class Users
 {
 
-	protected static connect(){
+	protected static void connect(){
 		if (!Base.hasConnection()){
 			Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/Ingenieria", "reversi", "123");
 		}		
